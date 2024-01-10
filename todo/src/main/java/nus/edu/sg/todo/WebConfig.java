ea66a79ab5550706a0085bd84619b86c463ff737
Irudaya Raj Lourdhu <irudayambics@gmail.com>
2024-01-10 16:32:43 +0800

    Fix cors
    Testing

--- todo/src/main/java/nus/edu/sg/todo/WebConfig.java
+++ todo/src/main/java/nus/edu/sg/todo/WebConfig.java
@@ -13,7 +13,8 @@ public class WebConfig {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("*").allowedOrigins("http://localhost:4200");
+				 registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS").allowedOrigins("*")
+                 .allowedHeaders("*");
 			}
 		};
 	}
